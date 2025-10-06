0c4d798d418979574846c4bdc05de70258427dd4
heman chandra <hemanchandra701@gmail.com>
2025-10-06 17:15:48 +0530

    roboshop

--- 11-frontend.sh
+++ 11-frontend.sh
@@ -6,25 +6,27 @@ LocScript=$PWD
 Logs_Folder="/var/log/shell-roboshop"
 Script_Name=$( echo $0 | cut -d "." -f1 )
 Logs_File="$Logs_Folder/$Script_Name.log"
-Start_Time=$( date +%s )
+
+
 
 mkdir -p $Logs_Folder
 echo "Script Started at : $(date)" | tee -a $Logs_File
 
 #Root=0,other than 0 =Normal user
 if [ $UserId -ne 0 ];then
-    echo  "Take Root Access To run this Shell Script"
+    echo -e "Take Root Access To run this Shell Script"
     exit 1
 fi
 
 Validation(){
     if [ $1 -ne 0 ];then
-        echo -e "$2 ... Failed! $N" | tee -a $Logs_File
+        echo -e "$2  Failed! $N" | tee -a $Logs_File
         exit 1
     else
-        echo -e "$2 ... Successfully! $N" | tee -a  $Logs_File
+        echo -e "$2  Successfully! $N" | tee -a  $Logs_File
     fi
 }
+
 
 dnf module disable nginx -y &>>$Logs_File
 Validation $? "Disable Nginx"
@@ -38,40 +40,24 @@ Validation $? "Install Nginx"
 systemctl enable nginx &>>$Logs_File
 Validation $? "Enable Nginx" 
 
-systemctl start nginx &>>$Logs_File
+systemctl start nginx 
 Validation $? "Start Nginx"
 
-rm -rf /usr/share/nginx/html/* &>>$Logs_File
+rm -rf /usr/share/nginx/html/* 
 Validation $? "Remove Default Html"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$Logs_File
-Validation $? "Download Frontend"
+Validation $? "Download Frontend Zip"
 
-cd /usr/share/nginx/html &>>$Logs_File
-Validation $? "Move to Nginx Dir"
+cd /usr/share/nginx/html 
+Validation $? "Change Dir to /usr/share/nginx/html"
 
 unzip /tmp/frontend.zip &>>$Logs_File
-Validation $? "Unzip Frontend"
+Validation $? "Download Frontend Unzip"
 
 cp $LocScript/nginx.conf /etc/nginx/nginx.conf &>>$Logs_File
 Validation $? "Nginx Conf Setup"
 
-systemctl restart nginx &>>$Logs_File
+systemctl restart nginx
 Validation $? "Restart Nginx"
-
-
-End_Time=$(date +%s)
-Total_Time=$(( $End_Time - $Start_Time ))
-echo " Taken by script to Execute is : $Total_Time "
-
-
-
-
-
-
-
-
-
-
-
 
