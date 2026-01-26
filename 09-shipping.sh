9ab53b51dc74d886e014bd91a3757c064c1666cd
heman chandra <hemanchandra701@gmail.com>
2026-01-26 21:29:04 +0530

    shell-roboshop-common

--- 09-shipping.sh
+++ 09-shipping.sh
@@ -21,7 +21,7 @@ mysql -h $Mysql_Host -uroot -pRoboShop@1 -e "use cities" &>>$Logs
 if [ $? -ne 0 ];then
     mysql -h $Mysql_Host -uroot -pRoboShop@1 < /app/db/schema.sql &>>$Logs
     mysql -h $Mysql_Host -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$Logs
-    mysql -h $Mysql_Host -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$Logs
+    mysql -h $Mysql_Host -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$Logs
 else
     echo -e "Already Loaded into Shipping ... $Y SKIPPING $N" | tee -a $Logs
 fi
